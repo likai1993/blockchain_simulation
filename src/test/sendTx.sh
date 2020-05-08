7a380e03a39f06fc2e147c5d72a5f9750afc65c0
anonymous-submission <kai@sgx190>
2020-05-07 21:53:21 -0400

    test script

--- src/test/sendTx.sh
+++ src/test/sendTx.sh
@@ -6,4 +6,5 @@ amount=$4
 subnet=$5
 
 curl -X POST --data '{"jsonrpc":"2.0","id":2,"method":"unlock","params":['\"$name\"','\"$psw\"']}' "http://192.168.$subnet:8888"
+echo -e "\n"
 curl -X POST --data '{"jsonrpc":"2.0","id":2,"method":"sendTransaction","params":['\"$receiver\"','\"$amount\"']}' "http://192.168.$subnet:8888"
